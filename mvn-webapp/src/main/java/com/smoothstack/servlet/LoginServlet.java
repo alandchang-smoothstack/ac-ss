217c6af8114de3e5463431ed78eaee980c357ed3
Aland <aland.chang@smoothstack.com>
2020-02-13 15:31:13 -0500

    .

--- mvn-webapp/src/main/java/com/smoothstack/servlet/LoginServlet.java
+++ mvn-webapp/src/main/java/com/smoothstack/servlet/LoginServlet.java
@@ -1,7 +1,6 @@
 package com.smoothstack.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
 import com.smoothstack.dto.User;
 
 @WebServlet("/login")
@@ -19,16 +17,6 @@ public class LoginServlet extends HttpServlet {
 
 	public LoginServlet() {
 		super();
-	}
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		User user = new User();
-		user.setName(request.getParameter("name"));
-		user.setPassword(request.getParameter("password"));
-		PrintWriter out = response.getWriter();
-		out.print(new Gson().toJson(user));
-		out.flush();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
